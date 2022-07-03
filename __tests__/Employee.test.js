5d04980c6672e8ebc8d327d268a9439b4f0e2ba8
charlie <gn4rtistic@gmail.com>
2022-07-03 00:06:37 -0700

    employee.js written, easy peasy, short and simple. back to testing... whoo

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,44 +1,44 @@
-const Employee = require("../lib.Employee");
+const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
     // creates a test for employee
     test('should return an object containing "employeeName, id, email" property when called with the "new" keyword',
         () => {
-            const object = new Employee();
+            const obj = new Employee();
             //employeeName, id, email, avatar
-            expect("employeeName" in object).toEqual(true);
-            expect("id" in object).toEqual(true);
-            expect("email" in object).toEqual(true);
+            expect("employeeName" in obj).toEqual(true);
+            expect("id" in obj).toEqual(true);
+            expect("email" in obj).toEqual(true);
         })
     
     // test to see if type of new emploee is an object
     test('should become an object when use new employee', () => {
-        const object = new Employee();
+        const obj = new Employee();
 
-        expect(typeof object).toEqual("object");
+        expect(typeof obj).toEqual("object");
     })
 
     // test if i can set name via constructor
     test('should set employee name equal to first parameter', () => {
         const test = "Tanjiro"; // demon slayer hehehehe
-        const object = new Employee(test);
+        const obj = new Employee(test);
 
-        expect(object.employeeName).toEqual(test);
+        expect(obj.employeeName).toEqual(test);
     })
 
     // test if I can set id via constructor
     test('should set employee id equal to second parameter', () => {
         const test = "2222"; //lucky angel numbers, hope this test works aha
-        const object = new Employee('name', test);
+        const obj = new Employee('name', test);
 
-        expect(object.employeeName).toEqual(test);
+        expect(obj.id).toEqual(test);
     })
 
     // test to see if i can set email via constructor
     test('should set employee email equal to third parameter', () => {
         const test = "iLoveTacos@gmail.com" // not a lie, the tacos in arizona just dont smack like the ones in san antonio :(
-        const object = new Employee('email', 'id', test);
+        const obj = new Employee('email', 'id', test);
 
-        expect(object.email).toEqual(test);
+        expect(obj.email).toEqual(test);
     })
 })
