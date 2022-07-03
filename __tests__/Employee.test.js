dafdc0a0630691f6a0b0e0e8b0952e83e90c8f00
charlie <gn4rtistic@gmail.com>
2022-07-03 00:23:26 -0700

    complete testing for Employee, moving onwardgit status

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    // creates a test for employee
+    // creates a tests for employee//
     test('should return an object containing "employeeName, id, email" property when called with the "new" keyword',
         () => {
             const obj = new Employee();
@@ -36,9 +36,50 @@ describe("Employee", () => {
 
     // test to see if i can set email via constructor
     test('should set employee email equal to third parameter', () => {
-        const test = "iLoveTacos@gmail.com" // not a lie, the tacos in arizona just dont smack like the ones in san antonio :(
+        const test = "iLoveTacos@whyImFat.com" // not a lie, the tacos in arizona just dont smack like the ones in san antonio :(
         const obj = new Employee('email', 'id', test);
 
         expect(obj.email).toEqual(test);
     })
 })
+
+
+// creating tests for methods //
+
+//test to see if class name returns "Employee"
+test('should become an object when i use new employee', () => {
+    const obj = new Employee();
+
+    expect(obj.constructor.name).toEqual("Employee");
+})
+
+//test to see if getName() returns a name
+test('should return the employee name when get name is called', () => {
+    const test = "Yuji"; // jujitsu kaisen ref
+    const obj = new Employee(test);
+
+    expect(obj.getName()).toEqual(test);
+})
+
+// test to see if getId() returns their ID
+test('should return employees ID number when called', () => {
+    const test = "7777"; // now THIS mf is lucky af
+    const obj = new Employee("name", test);
+
+    expect(obj.getId()).toEqual(test);
+})
+
+//test to see if getEmail returns their email
+test('should return the employees email when called', () => {
+    const test = "iHateAvacados@plsDontJudgeMe.com" // i really do, whoever called them nature's butter was wrong
+    const obj = new Employee("name", "id", test);
+
+    expect(obj.getEmail()).toEqual(test);
+})
+
+//test to see if getRole() returns their role
+test('should return employees role when called', () => {
+    const obj = new Employee;
+
+    expect(obj.getRole()).toEqual("Employee");
+});
